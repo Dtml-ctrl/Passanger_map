76844aa856a48044b8a8029de0830949b8988fec
Dtml-ctrl <dnew0010@student.monash.edu>
2024-09-29 22:34:02 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "Passanger_Numbers_Map.vega";
+vegaEmbed("#Station_Map", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
