e5400f386dc3d060b14cbe6805dad98b0bdf7241
Dtml-ctrl <dnew0010@student.monash.edu>
2024-10-16 20:03:37 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,13 @@ vegaEmbed("#Station_Map", vg_1).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_2 = "Metro_Bar.vega";
+vegaEmbed("#Metro_Bar", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_3 = "Regional_Bar.vega";
+vegaEmbed("#Regional_Bar", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
